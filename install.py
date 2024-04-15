e82e42fa20dbcd792f4298ed5b3ae5e2b8597cc4
w-e-w <40751091+w-e-w@users.noreply.github.com>
2024-04-15 14:00:14 +0900

    delete __pycache__

--- install.py
+++ install.py
@@ -2,9 +2,9 @@ import launch
 
 if not launch.is_installed("torchmcubes"):
     launch.run_pip("install git+https://github.com/tatsy/torchmcubes.git", "requirements for torchmcubes")
-else:
-    launch.run_pip("uninstall torchmcubes", desc="Uninstall torchmcubes")
-    launch.run_pip("install git+https://github.com/tatsy/torchmcubes.git", "requirements for torchmcubes")
+# else:
+#     launch.run_pip("uninstall torchmcubes", desc="Uninstall torchmcubes")
+#     launch.run_pip("install git+https://github.com/tatsy/torchmcubes.git", "requirements for torchmcubes")
 
 if not launch.is_installed("rembg"):
     launch.run_pip("install rembg", "requirements for rembg")
