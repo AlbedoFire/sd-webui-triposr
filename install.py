c34902741b091fe95db6ae426eef4944fef5132b
w-e-w <40751091+w-e-w@users.noreply.github.com>
2024-04-15 14:03:33 +0900

    don't uninstall torchmcubes

--- install.py
+++ install.py
@@ -2,10 +2,6 @@ import launch
 
 if not launch.is_installed("torchmcubes"):
     launch.run_pip("install git+https://github.com/tatsy/torchmcubes.git", "requirements for torchmcubes")
-# else:
-#     launch.run_pip("uninstall torchmcubes", desc="Uninstall torchmcubes")
-#     launch.run_pip("install git+https://github.com/tatsy/torchmcubes.git", "requirements for torchmcubes")
-
 if not launch.is_installed("rembg"):
     launch.run_pip("install rembg", "requirements for rembg")
 if not launch.is_installed("trimesh"):
